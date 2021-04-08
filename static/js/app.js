67c2b1bd66dc4124132c2b85c207334d320dd2de
zernach <ryan@zernach.com>
2021-04-08 13:53:42 -0400

    adds demoinfobox d3 select

--- static/js/app.js
+++ static/js/app.js
@@ -11,13 +11,18 @@ function buildCharts(UID) {
 };
 
 // FUNCTION #2 of 4
-function populateDemoInfo(UID){
+function populateDemoInfo(UID) {
+
     var demographicInfoBox = d3.select("#sample-metadata");
-    console.log(UID)
+
+    d3.json("samples.json").then(data => {
+        console.log(data)
+    })
 }
 
 // FUNCTION #3 of 4
 function optionChanged(UID) {
+    console.log(UID);
     buildCharts(UID);
     populateDemoInfo(UID);
 }
